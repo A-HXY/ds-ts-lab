8ff5145044db2e7626e0765ed32292430e03285b
Xinyue <858937756@qq.com>
2025-02-02 23:18:18 +0000

    Added type-safety to colleague data.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -19,8 +19,16 @@ const friend1 = {
   const friends = [friend1, friend2];
   console.log(friends[1]);
   
-  //   -------------------
-  const colleague1 = {
+  interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+      email: string;
+      extension: number;
+    };
+  }
+  
+  const colleague1: Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -29,7 +37,7 @@ const friend1 = {
     },
   };
   
-  const colleague2 = {
+  const colleague2: Colleague = {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -38,7 +46,7 @@ const friend1 = {
     },
   };
   
-  const colleague3 = {
+  const colleague3: Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
@@ -46,6 +54,7 @@ const friend1 = {
       extension: 125,
     },
   };
+  
   const colleagues = {
     current: [colleague1, colleague2, colleague3],
     former: [],
